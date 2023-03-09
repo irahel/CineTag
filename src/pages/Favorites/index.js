788e874f78b1f7ec75ba50dd4a7c35527e644f7e
Irahel <isaac_rahel@live.com>
2023-03-09 11:20:33 -0300

    feat: apply context to fav pag

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -3,8 +3,11 @@ import styles from "./Favorites.module.css";
 import Banner from "components/Banner";
 import Title from "components/Title";
 import Card from "components/Card";
+import { useFavorites } from 'contexts/FavoritesContext';
 
 function Favorites() {
+  const { favorites } = useFavorites();
+
   return (
     <>
     <Banner img='favoritos'/>
@@ -12,7 +15,9 @@ function Favorites() {
         <h1>Meus Favoritos</h1>
     </Title>
     <section className={styles.container}>
-        <Card id="1" title="Sony" banner="https://github.com/irahel.png"/>
+      {favorites.map((fav) => (
+        <Card key={fav.id} id={fav.id} title={fav.title} banner={fav.banner}/>
+      ))}
 
     </section>
     </>
